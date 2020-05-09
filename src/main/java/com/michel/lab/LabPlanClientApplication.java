6e3ee90b9f05972ea19eb24e9117ecc0095236eb
Senpothia <kintelalpz@gmail.com>
2020-05-09 09:54:45 +0200

    init

---
+++ src/main/java/com/michel/lab/LabPlanClientApplication.java
@@ -0,0 +1,13 @@
+package com.michel.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabPlanClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabPlanClientApplication.class, args);
+	}
+
+}
