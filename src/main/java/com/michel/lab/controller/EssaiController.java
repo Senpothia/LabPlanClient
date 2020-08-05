fa79b6db4dac88b9705ab550bbb23559c20dddc7
Senpothia <kintelalpz@gmail.com>
2020-08-05 08:59:54 +0200

    Correction enregistrement sélection procédure méthode choisirEssais2()

--- src/main/java/com/michel/lab/controller/EssaiController.java
+++ src/main/java/com/michel/lab/controller/EssaiController.java
@@ -145,6 +145,7 @@ public class EssaiController {
 		redirectAttributes.addAttribute("id", id);
 		redirectAttributes.addAttribute("domaine", domaineId);
 		redirectAttributes.addAttribute("qualification", qualification);
+		
 		return "redirect:/labplan/private/essai/procedures";
 	}
 
@@ -158,39 +159,55 @@ public class EssaiController {
 		Utilisateur utilisateur = userConnexion.obtenirUtilisateur(session, model);
 		System.out.println("*** méthode choisoirEssai2 *** ");
 		System.out.println("Avant");
-		System.out.println("id domaine: " + id);
+		System.out.println("id domaine: " + domaineId);
 		System.out.println("id qualification: " + qualification);
 
-		List<ProcedureAux> procedures0 = microServiceLab.obtenirProceduresParDomaine(id);  // liste de toutes les procédures du domaine
+		List<ProcedureAux> procedures0 = microServiceLab.obtenirProceduresParDomaine(domaineId);  // liste de toutes les procédures du domaine
 		List<Integer> listeIdProcedures = new ArrayList<Integer>();
 
-		for (ProcedureAux pro : procedures0) {
+		for (ProcedureAux pro : procedures0) {  // Récupération de tous les id de toutes les procédures existantes dans le domaine
 
 			Integer idPro = pro.getId();
 			listeIdProcedures.add(idPro);
 		}
-
-		Groupe groupe = new Groupe(id, qualification);
+		
+		System.out.println("Taille liste listeIdProcedure: " + listeIdProcedures.size());
+		
+		Groupe groupe = new Groupe(domaineId, qualification);
 		List<Integer> idProcedures = microServiceLab.obtenirSelectionProcedure(groupe); // procédures sélectionnées pour
 																						// la qualification
-		List<ProcedureAux> procedures = new ArrayList<ProcedureAux>();
-		boolean isPresent ;
-		for (ProcedureAux pro : procedures0) {
-
-			Integer identifiantProcedure = pro.getId();
-			System.out.println("identifiantProcedure: " + identifiantProcedure);
-			isPresent = idProcedures.contains(identifiantProcedure);
+		System.out.println("Taille liste des procédures sélectionnées pour la qualification choisie: " + idProcedures.size());
+		
+		for(Integer iden: idProcedures) {
 			
-			if (isPresent) {
+			System.out.println("identifiant de procédure sélectionné: " + iden);
+		}
+		List<ProcedureAux> procedures = new ArrayList<ProcedureAux>();
+		//boolean isPresent ;
+		
+		for (Integer proId : listeIdProcedures) {
+
+			//Integer identifiantProcedure = proId;
+			System.out.println("identifiantProcedure boucle for: " + proId);
+		//	isPresent = idProcedures.contains(identifiantProcedure);
+			
+			if (idProcedures.contains(proId)) {
 				
-				pro.setActif(true);
+				ProcedureAux procedure = procedures0.get(listeIdProcedures.indexOf(proId));
+				procedure.setActif(false);
+				procedures.add(procedure);
+				//pro.setActif(true);
 				
 			} else {
 				
-				pro.setActif(true);
+				ProcedureAux procedure = procedures0.get(listeIdProcedures.indexOf(proId));
+				procedure.setActif(true);
+				procedures.add(procedure);
+				//pro.setActif(false);
 			}
 			
-			procedures.add(pro);
+			//procedures.add(pro);
+			
 
 		}
 		
@@ -198,12 +215,14 @@ public class EssaiController {
 			
 			System.out.println(pro.toString());
 		}
+		
 		System.out.println("taille liste procedures : " + procedures0.size());
 		System.out.println("taille liste idProcedure des id de procédures sélectionnées : " + idProcedures.size());
+		
 		model.addAttribute("procedures", procedures);
 		model.addAttribute("qualification", qualification);
 		model.addAttribute("utilisateur", utilisateur);
-		model.addAttribute("domaine", id);
+		model.addAttribute("domaine", domaineId);
 		return Constants.ESSAIS;
 
 	}
