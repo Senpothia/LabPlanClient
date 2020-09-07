a9bd328402f295cf3f38a3f127ce4687a4b8e8ee
Senpothia <kintelalpz@gmail.com>
2020-09-07 08:43:31 +0200

    Ajout coloration des entêtes de table

--- src/main/java/com/michel/lab/view/pdf/RapportViewPdf.java
+++ src/main/java/com/michel/lab/view/pdf/RapportViewPdf.java
@@ -1,5 +1,6 @@
 package com.michel.lab.view.pdf;
 
+import java.awt.Color;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -29,6 +30,7 @@ import com.lowagie.text.Image;
 import com.lowagie.text.Paragraph;
 import com.lowagie.text.Phrase;
 import com.lowagie.text.pdf.BaseFont;
+import com.lowagie.text.pdf.PdfPCell;
 import com.lowagie.text.pdf.PdfPTable;
 import com.lowagie.text.pdf.PdfTable;
 import com.lowagie.text.pdf.PdfWriter;
@@ -161,12 +163,33 @@ public class RapportViewPdf extends AbstractPdfView {
 		PdfPTable tableEchantillon = new PdfPTable(6);
 		tableEchantillon.setWidths(new float[] { 1, 1, 1.5f, 1, 3, 1 });
 
-		tableEchantillon.addCell("Ordre");
-		tableEchantillon.addCell("Numéro");
-		tableEchantillon.addCell("Date");
-		tableEchantillon.addCell("Version");
-		tableEchantillon.addCell("Caractéristique");
-		tableEchantillon.addCell("Statut");
+		
+		PdfPCell cell1 = new PdfPCell(new Paragraph("Ordre"));
+		cell1.setBackgroundColor(new Color( 190, 58, 220 ));
+		
+		PdfPCell cell2 = new PdfPCell(new Paragraph("Numéro"));
+		cell2.setBackgroundColor(new Color( 190, 58, 220 ));
+		
+		PdfPCell cell3 = new PdfPCell(new Paragraph("Date"));
+		cell3.setBackgroundColor(new Color( 190, 58, 220 ));
+		
+		PdfPCell cell4 = new PdfPCell(new Paragraph("Version"));
+		cell4.setBackgroundColor(new Color( 190, 58, 220 ));
+		
+		PdfPCell cell5 = new PdfPCell(new Paragraph("Caractéristique"));
+		cell5.setBackgroundColor(new Color( 190, 58, 220 ));
+		
+		PdfPCell cell6 = new PdfPCell(new Paragraph("Statut"));
+		cell6.setBackgroundColor(new Color( 190, 58, 220 ));
+		
+		tableEchantillon.addCell(cell1);
+		tableEchantillon.addCell(cell2);
+		tableEchantillon.addCell(cell3);
+		tableEchantillon.addCell(cell4);
+		tableEchantillon.addCell(cell5);
+		tableEchantillon.addCell(cell6);
+		
+		
 		int i = 1;
 		for (EchantillonAux e : echantillons) {
 
@@ -201,12 +224,42 @@ public class RapportViewPdf extends AbstractPdfView {
 			PdfPTable esTable = new PdfPTable(6);
 			esTable.setWidths(new float[] { 0.5f, 3.5f, 1, 1, 1, 1 });
 
+			PdfPCell cell11 = new PdfPCell(new Paragraph("N°"));
+			cell11.setBackgroundColor(new Color( 46, 127, 238 ));
+			
+			PdfPCell cell12 = new PdfPCell(new Paragraph("Essai"));
+			cell12.setBackgroundColor(new Color( 46, 127, 238 ));
+			
+			PdfPCell cell13 = new PdfPCell(new Paragraph("Version"));
+			cell13.setBackgroundColor(new Color( 46, 127, 238 ));
+		
+			PdfPCell cell14 = new PdfPCell(new Paragraph("Domaine"));
+			cell14.setBackgroundColor(new Color( 46, 127, 238 ));
+
+			
+			PdfPCell cell15 = new PdfPCell(new Paragraph("Statut"));
+			cell15.setBackgroundColor(new Color( 46, 127, 238 ));
+			
+			PdfPCell cell16 = new PdfPCell(new Paragraph("Résultat"));
+			cell16.setBackgroundColor(new Color( 46, 127, 238 ));
+
+			/*
+
 			esTable.addCell("N°");
 			esTable.addCell("Essai");
 			esTable.addCell("Version");
 			esTable.addCell("Domaine");
 			esTable.addCell("Statut");
 			esTable.addCell("Résultat");
+			
+			*/
+			
+			esTable.addCell(cell11);
+			esTable.addCell(cell12);
+			esTable.addCell(cell13);
+			esTable.addCell(cell14);
+			esTable.addCell(cell15);
+			esTable.addCell(cell16);
 			int j = 1;
 
 			esTable.addCell(es.getNumero().toString());
@@ -225,48 +278,95 @@ public class RapportViewPdf extends AbstractPdfView {
 					new Chunk("    Séquences", FontFactory.getFont(FontFactory.TIMES_ROMAN, 18)));
 			sequenceTitre.setSpacingAfter(30);
 			document.add(sequenceTitre);
+
 			int k = 1;
-			
+
 			for (SequenceAux s : sequences) {
-/*
-				PdfPTable seqTable = new PdfPTable(10);
-				seqTable.setWidths(new float[] { 1, 2f, 2.5f, 3.0f, 3.0f, 2.5f, 2.0f, 5.0f, 2.0f, 2.5f });
+				/*
+				 * PdfPTable seqTable = new PdfPTable(10); seqTable.setWidths(new float[] { 1,
+				 * 2f, 2.5f, 3.0f, 3.0f, 2.5f, 2.0f, 5.0f, 2.0f, 2.5f });
+				 * 
+				 * seqTable.addCell(new Phrase("N°",
+				 * FontFactory.getFont(FontFactory.TIMES_ROMAN, 9))); seqTable.addCell(new
+				 * Phrase("Nom", FontFactory.getFont(FontFactory.TIMES_ROMAN, 9)));
+				 * seqTable.addCell(new Phrase("Niveau",
+				 * FontFactory.getFont(FontFactory.TIMES_ROMAN, 9))); seqTable.addCell(new
+				 * Phrase("Début", FontFactory.getFont(FontFactory.TIMES_ROMAN, 9)));
+				 * seqTable.addCell(new Phrase("Fin",
+				 * FontFactory.getFont(FontFactory.TIMES_ROMAN, 9))); seqTable.addCell(new
+				 * Phrase("Durée", FontFactory.getFont(FontFactory.TIMES_ROMAN, 9)));
+				 * seqTable.addCell(new Phrase("Profil",
+				 * FontFactory.getFont(FontFactory.TIMES_ROMAN, 9))); seqTable.addCell(new
+				 * Phrase("Commentaire", FontFactory.getFont(FontFactory.TIMES_ROMAN, 9)));
+				 * seqTable.addCell(new Phrase("Statut",
+				 * FontFactory.getFont(FontFactory.TIMES_ROMAN, 9))); seqTable.addCell(new
+				 * Phrase("Résultat", FontFactory.getFont(FontFactory.TIMES_ROMAN, 9))); int k =
+				 * 1;
+				 * 
+				 * //seqTable.addCell(String.valueOf(k)); seqTable.addCell(new
+				 * Phrase(String.valueOf(k), FontFactory.getFont(FontFactory.TIMES_ROMAN, 10)));
+				 * seqTable.addCell(new Phrase(s.getNom(),
+				 * FontFactory.getFont(FontFactory.TIMES_ROMAN, 10))); seqTable.addCell(new
+				 * Phrase(s.getNiveau(), FontFactory.getFont(FontFactory.TIMES_ROMAN, 10)));
+				 * seqTable.addCell(new Phrase(s.getDebutText(),
+				 * FontFactory.getFont(FontFactory.TIMES_ROMAN, 10))); seqTable.addCell(new
+				 * Phrase(s.getFinText(), FontFactory.getFont(FontFactory.TIMES_ROMAN, 10)));
+				 * seqTable.addCell(new Phrase("Durée",
+				 * FontFactory.getFont(FontFactory.TIMES_ROMAN, 10))); seqTable.addCell(new
+				 * Phrase(s.getProfil(), FontFactory.getFont(FontFactory.TIMES_ROMAN, 10)));
+				 * seqTable.addCell(new Phrase(s.getCommentaire(),
+				 * FontFactory.getFont(FontFactory.TIMES_ROMAN, 10))); seqTable.addCell(new
+				 * Phrase(s.getActif(), FontFactory.getFont(FontFactory.TIMES_ROMAN, 10)));
+				 * seqTable.addCell(new Phrase( s.getAvis(),
+				 * FontFactory.getFont(FontFactory.TIMES_ROMAN, 10)));
+				 * 
+				 */
 
-				seqTable.addCell(new Phrase("N°", FontFactory.getFont(FontFactory.TIMES_ROMAN, 9)));
-				seqTable.addCell(new Phrase("Nom", FontFactory.getFont(FontFactory.TIMES_ROMAN, 9)));
-				seqTable.addCell(new Phrase("Niveau", FontFactory.getFont(FontFactory.TIMES_ROMAN, 9)));
-				seqTable.addCell(new Phrase("Début", FontFactory.getFont(FontFactory.TIMES_ROMAN, 9)));
-				seqTable.addCell(new Phrase("Fin", FontFactory.getFont(FontFactory.TIMES_ROMAN, 9)));
-				seqTable.addCell(new Phrase("Durée", FontFactory.getFont(FontFactory.TIMES_ROMAN, 9)));
-				seqTable.addCell(new Phrase("Profil", FontFactory.getFont(FontFactory.TIMES_ROMAN, 9)));
-				seqTable.addCell(new Phrase("Commentaire", FontFactory.getFont(FontFactory.TIMES_ROMAN, 9)));
-				seqTable.addCell(new Phrase("Statut", FontFactory.getFont(FontFactory.TIMES_ROMAN, 9)));
-				seqTable.addCell(new Phrase("Résultat", FontFactory.getFont(FontFactory.TIMES_ROMAN, 9)));
-				int k = 1;
+				System.out.println("Numéro de page: " + writer.getPageNumber() + " k: " + k);
 
-				//seqTable.addCell(String.valueOf(k));
-				seqTable.addCell(new Phrase(String.valueOf(k), FontFactory.getFont(FontFactory.TIMES_ROMAN, 10)));
-				seqTable.addCell(new Phrase(s.getNom(), FontFactory.getFont(FontFactory.TIMES_ROMAN, 10)));
-				seqTable.addCell(new Phrase(s.getNiveau(), FontFactory.getFont(FontFactory.TIMES_ROMAN, 10)));
-				seqTable.addCell(new Phrase(s.getDebutText(), FontFactory.getFont(FontFactory.TIMES_ROMAN, 10)));
-				seqTable.addCell(new Phrase(s.getFinText(), FontFactory.getFont(FontFactory.TIMES_ROMAN, 10)));
-				seqTable.addCell(new Phrase("Durée", FontFactory.getFont(FontFactory.TIMES_ROMAN, 10)));
-				seqTable.addCell(new Phrase(s.getProfil(), FontFactory.getFont(FontFactory.TIMES_ROMAN, 10)));
-				seqTable.addCell(new Phrase(s.getCommentaire(), FontFactory.getFont(FontFactory.TIMES_ROMAN, 10)));
-				seqTable.addCell(new Phrase(s.getActif(), FontFactory.getFont(FontFactory.TIMES_ROMAN, 10)));
-				seqTable.addCell(new Phrase( s.getAvis(), FontFactory.getFont(FontFactory.TIMES_ROMAN, 10)));
+				if (writer.getPageNumber() == 3) {
 
- 				*/
+					System.out.println("saut de page");
+					// document.newPage();
+
+					Paragraph margeSup2 = new Paragraph(
+							new Chunk(" ", FontFactory.getFont(FontFactory.TIMES_ROMAN, 24)));
+					margeSup2.setAlignment(Element.ALIGN_CENTER);
+					margeSup2.setSpacingAfter(20);
+					document.add(margeSup2);
+
+				}
+
+				/*
+				 * if (writer.getPageNumber() > 2 && k == 2) { // trouver les multiples de 3
+				 * 
+				 * document.newPage();
+				 * 
+				 * Paragraph margeSup2 = new Paragraph( new Chunk(" ",
+				 * FontFactory.getFont(FontFactory.TIMES_ROMAN, 24)));
+				 * margeSup2.setAlignment(Element.ALIGN_CENTER); margeSup2.setSpacingAfter(60);
+				 * document.add(margeSup2);
+				 * 
+				 * }
+				 * 
+				 */
 				
+				PdfPCell cell21 = new PdfPCell(new Paragraph("N°"));
+				cell21.setBackgroundColor(new Color(17, 142, 46));
+				
+				PdfPCell cell22 = new PdfPCell(new Phrase(String.valueOf(k), FontFactory.getFont(FontFactory.TIMES_ROMAN, 10)));
+				cell22.setBackgroundColor(new Color(17, 142, 46));
+				
+
 				PdfPTable seqTable = new PdfPTable(2);
-				seqTable.setWidths(new float[] { 1, 2f});
-				
-				seqTable.addCell(new Phrase("N°", FontFactory.getFont(FontFactory.TIMES_ROMAN, 9)));
-				
-				seqTable.addCell(new Phrase(String.valueOf(k), FontFactory.getFont(FontFactory.TIMES_ROMAN, 10)));
-				seqTable.addCell(new Phrase("Nom", FontFactory.getFont(FontFactory.TIMES_ROMAN, 9)));
-				seqTable.addCell(new Phrase(s.getNom(), FontFactory.getFont(FontFactory.TIMES_ROMAN, 10)));
-				
+				seqTable.setWidths(new float[] { 1, 3f });
+
+				//seqTable.addCell(new Phrase("N°", FontFactory.getFont(FontFactory.TIMES_ROMAN, 9)));
+
+				//seqTable.addCell(new Phrase(String.valueOf(k), FontFactory.getFont(FontFactory.TIMES_ROMAN, 10)));
+				seqTable.addCell(cell21);
+				seqTable.addCell(cell22);
+
 				seqTable.addCell(new Phrase("Niveau", FontFactory.getFont(FontFactory.TIMES_ROMAN, 9)));
 				seqTable.addCell(new Phrase(s.getNiveau(), FontFactory.getFont(FontFactory.TIMES_ROMAN, 10)));
 				seqTable.addCell(new Phrase("Début", FontFactory.getFont(FontFactory.TIMES_ROMAN, 9)));
@@ -282,36 +382,29 @@ public class RapportViewPdf extends AbstractPdfView {
 				seqTable.addCell(new Phrase("Statut", FontFactory.getFont(FontFactory.TIMES_ROMAN, 9)));
 				seqTable.addCell(new Phrase(s.getActif(), FontFactory.getFont(FontFactory.TIMES_ROMAN, 10)));
 				seqTable.addCell(new Phrase("Résultat", FontFactory.getFont(FontFactory.TIMES_ROMAN, 9)));
-				seqTable.addCell(new Phrase( s.getAvis(), FontFactory.getFont(FontFactory.TIMES_ROMAN, 10)));
-				
-				///////////////////////////////
+				seqTable.addCell(new Phrase(s.getAvis(), FontFactory.getFont(FontFactory.TIMES_ROMAN, 10)));
+
 				seqTable.setSpacingAfter(20);
 				document.add(seqTable);
 				k++;
 			}
 
 			document.newPage();
-			
+
 			Paragraph margeSup2 = new Paragraph(new Chunk(" ", FontFactory.getFont(FontFactory.TIMES_ROMAN, 24)));
 			margeSup2.setAlignment(Element.ALIGN_CENTER);
 			margeSup2.setSpacingAfter(-20);
 			document.add(margeSup2);
-			
-		
 
 		}
-		
-		Paragraph avisTitre = new Paragraph(
-				new Chunk("4. Avis", FontFactory.getFont(FontFactory.TIMES_ROMAN, 24)));
+
+		Paragraph avisTitre = new Paragraph(new Chunk("4. Avis", FontFactory.getFont(FontFactory.TIMES_ROMAN, 24)));
 		avisTitre.setSpacingAfter(20);
 		document.add(avisTitre);
-		
-		Paragraph avis = new Paragraph(
-				new Chunk(rapport.getAvis(), FontFactory.getFont(FontFactory.TIMES_ROMAN, 12)));
+
+		Paragraph avis = new Paragraph(new Chunk(rapport.getAvis(), FontFactory.getFont(FontFactory.TIMES_ROMAN, 12)));
 		avis.setSpacingAfter(20);
 		document.add(avis);
-		
-		
 
 	}
 
