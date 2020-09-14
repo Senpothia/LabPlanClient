e07ef42a7632f1b38f38986a0b2369e651ca5d1e
Senpothia <kintelalpz@gmail.com>
2020-09-14 18:42:07 +0200

    Correction bug de redirection lors de la création / sppression d'une fiche hors et en dedans d'une qualification

--- src/main/java/com/michel/lab/controller/FicheController.java
+++ src/main/java/com/michel/lab/controller/FicheController.java
@@ -22,113 +22,138 @@ import com.michel.lab.service.UserConnexion;
 @Controller
 @RequestMapping("/labplan/private/fiche")
 public class FicheController {
-	
+
 	@Autowired
 	private UserConnexion userConnexion;
-	
+
 	@Autowired
 	private MicroServiceLab microServiceLab;
-	
-	
+
 	@GetMapping("/liste/{id}")
-	public String listesFiches(@PathVariable("id") Integer numQualification
-			, Model model, HttpSession session) {
-		
+	public String listesFiches(@PathVariable("id") Integer numQualification, Model model, HttpSession session) {
+
 		Utilisateur utilisateur = userConnexion.obtenirUtilisateur(session, model);
 		List<FicheAux> fiches = microServiceLab.voirLesFichesParQualification(numQualification);
 		model.addAttribute("fiches", fiches);
 		model.addAttribute("qualification", numQualification);
-		
+
 		return "fichesParQualification";
-		
+
 	}
-	
-	
+
 	@GetMapping("/creation")
 	public String listesFiches(Model model, HttpSession session) {
-		
+
 		Utilisateur utilisateur = userConnexion.obtenirUtilisateur(session, model);
 		model.addAttribute("formFiche", new FormFiche());
-		
-		
+
 		return "CreateFiche";
 	}
-	
+
 	@PostMapping("/creation")
 	public String listesFiches(Model model, HttpSession session, FormFiche formFiche) {
-		
+
 		Utilisateur utilisateur = userConnexion.obtenirUtilisateur(session, model);
 		formFiche.setAuteur(utilisateur.getId());
 		microServiceLab.enregistrerFiche(formFiche);
 		System.out.println("dégré: " + formFiche.getDegre());
 		List<FicheAux> fiches = microServiceLab.voirLesFiches();
 		model.addAttribute("fiches", fiches);
-		
+
 		return "fiches";
 	}
-	
+
 	@GetMapping("/voir")
 	public String voirLesFiches(Model model, HttpSession session) {
-		
+
 		Utilisateur utilisateur = userConnexion.obtenirUtilisateur(session, model);
 		List<FicheAux> fiches = microServiceLab.voirLesFiches();
 		model.addAttribute("fiches", fiches);
-		
+
 		return "fiches";
 	}
-	
+
 	@GetMapping("/ajouter/{qualification}")
-	public String ajouterFicheQualification(
-			@PathVariable("qualification") Integer numQualification,
-			Model model, HttpSession session) {
-		
+	public String ajouterFicheQualification(@PathVariable("qualification") Integer numQualification, Model model,
+			HttpSession session) {
+
 		System.out.println("numQual: " + numQualification);
 		Utilisateur utilisateur = userConnexion.obtenirUtilisateur(session, model);
 		FormFiche formFiche = new FormFiche();
 		formFiche.setQualification(numQualification);
-	
+
 		model.addAttribute("formFiche", formFiche);
 		model.addAttribute("qualification", numQualification);
-		
-		
+
 		return "ajouterFiche";
-		
+
 	}
-	
+
 	@PostMapping("/ajouter/{qualification}")
-	public String ajouterFiches(
-			@PathVariable("qualification") Integer numQualification,
-			Model model, HttpSession session, FormFiche formFiche) {
-		
+	public String ajouterFiches(@PathVariable("qualification") Integer numQualification, Model model,
+			HttpSession session, FormFiche formFiche) {
+
 		System.out.println("numQualif: " + numQualification);
 		Utilisateur utilisateur = userConnexion.obtenirUtilisateur(session, model);
 		formFiche.setAuteur(utilisateur.getId());
 		formFiche.setQualification(numQualification);
 		microServiceLab.ajouterFiche(formFiche);
-		
-		List<FicheAux> fiches = microServiceLab.voirLesFiches();
+
+		List<FicheAux> fiches = microServiceLab.voirLesFichesParQualification(numQualification);
 		model.addAttribute("fiches", fiches);
 		model.addAttribute("qualification", numQualification);
-		
+
 		return "fichesParQualification";
-		
-		
+
 	}
-	
-	@GetMapping("/voir/{id}")
-	public String voirLaFiche(
-			@PathVariable("id") Integer id,
+
+	@GetMapping("/ajouter")
+	public String ajouterFicheQualification(
+			// @PathVariable("qualification") Integer numQualification,
 			Model model, HttpSession session) {
-		
+
+		// System.out.println("numQual: " + numQualification);
+		Utilisateur utilisateur = userConnexion.obtenirUtilisateur(session, model);
+		FormFiche formFiche = new FormFiche();
+		// formFiche.setQualification(numQualification);
+
+		model.addAttribute("formFiche", formFiche);
+		model.addAttribute("qualification", 0);
+
+		return "creerFiche";
+
+	}
+
+	@PostMapping("/ajouter")
+	public String ajouterFiches(
+			// @PathVariable("qualification") Integer numQualification,
+			Model model, HttpSession session, FormFiche formFiche) {
+
+		// System.out.println("numQualif: " + numQualification);
+		Utilisateur utilisateur = userConnexion.obtenirUtilisateur(session, model);
+		formFiche.setAuteur(utilisateur.getId());
+		// formFiche.setQualification(numQualification);
+		microServiceLab.ajouterFiche(formFiche);
+
+		List<FicheAux> fiches = microServiceLab.voirLesFiches();
+		model.addAttribute("fiches", fiches);
+		// model.addAttribute("qualification", numQualification);
+
+		return "fiches";
+
+	}
+
+	@GetMapping("/voir/{id}")
+	public String voirLaFiche(@PathVariable("id") Integer id, Model model, HttpSession session) {
+
 		Utilisateur utilisateur = userConnexion.obtenirUtilisateur(session, model);
 		FicheAux fiche = microServiceLab.voirLaFiches(id);
-		
-		
+
 		model.addAttribute("fiche", fiche);
-		
+
 		return "fiche";
 	}
-	
+
 	@GetMapping("/supprimer/{id}")
 	public String supprimerLaFiche(
 			@PathVariable("id") Integer id,
@@ -140,15 +165,25 @@ public class FicheController {
 		System.out.println("numQualif pour suppression: " + numQualification);
 		microServiceLab.supprimerLaFiches(id);
 		
-		List<FicheAux> fiches = microServiceLab.voirLesFichesParQualification(numQualification);
-		model.addAttribute("fiches", fiches);
-		model.addAttribute("qualification", numQualification);
+		if(numQualification != null) {
+			
+			List<FicheAux> fiches = microServiceLab.voirLesFichesParQualification(numQualification);
+			model.addAttribute("fiches", fiches);
+			model.addAttribute("qualification", numQualification);
+			return "fichesParQualification";
+
+			
+		} else {
+			
+			List<FicheAux> fiches = microServiceLab.voirLesFiches();
+			model.addAttribute("fiches", fiches);
+			return"fiches";
+			
+		}
 		
-		return "fiches";
-		
+
 	}
-	
-	
+
 	@GetMapping("/modifier/{id}")
 	public String modifierLaFiche(
 			@PathVariable("id") Integer id,
@@ -183,8 +218,7 @@ public class FicheController {
 		return "modifierFiche";
 		
 	}
-	
-	
+
 	@PostMapping("/modifier/{id}")
 	public String enregistrerModificationFiche(
 			@PathVariable("id") Integer id,
@@ -207,6 +241,5 @@ public class FicheController {
 		return "fichesParQualification";
 		
 	}
-	
-	
+
 }
