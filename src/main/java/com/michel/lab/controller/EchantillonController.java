d1d478e50994f7273513195f0b32384cfc7e37fc
Senpothia <kintelalpz@gmail.com>
2021-03-14 11:57:40 +0100

    Modification sécurité des controller d'échantillons et essais

--- src/main/java/com/michel/lab/controller/EchantillonController.java
+++ src/main/java/com/michel/lab/controller/EchantillonController.java
@@ -37,159 +37,241 @@ public class EchantillonController {
 	public String creation(@PathVariable(name = "id") Integer id, Model model, HttpSession session) {
 
 		Utilisateur utilisateur = userConnexion.obtenirUtilisateur(session, model);
-		String token = (String) session.getAttribute("TOKEN");
-		token = "Bearer " + token;
-		model.addAttribute("formEchantillon", new FormEchantillon());
-		QualificationAux qualification = microServiceLab.obtenirQualification(token, id);
-		model.addAttribute("qualification", qualification);
-		return Constants.CREATION_ECHANTILLON;
+		
+		if (testUser(utilisateur)) {
+
+			String token = (String) session.getAttribute("TOKEN");
+			token = "Bearer " + token;
+			model.addAttribute("formEchantillon", new FormEchantillon());
+			QualificationAux qualification = microServiceLab.obtenirQualification(token, id);
+			model.addAttribute("qualification", qualification);
+			
+			return Constants.CREATION_ECHANTILLON;
+				
+
+			} else {
+
+				return "redirect:/labplan/connexion";
+			}
+		
+		
+		
 	}
 
 	@PostMapping("/creer/{id}") // id = numero qualification
 	public String enregistrementEchantillon(@PathVariable(name = "id") Integer id, Model model, HttpSession session,
 			FormEchantillon formEchantillon, RedirectAttributes redirectAttributes) {
-		
+
 		System.out.println("Numéro de qualification pour enregistrement ech: " + id);
 		Utilisateur utilisateur = userConnexion.obtenirUtilisateur(session, model);
-		String token = (String) session.getAttribute("TOKEN");
-		token = "Bearer " + token;
-		formEchantillon.setQualification(id);
-		System.out.println("Numéro de qualification pour enregistrement ech dans form: " + formEchantillon.getQualification());
-		microServiceLab.saveEchantillon(token, formEchantillon);
-
-		redirectAttributes.addAttribute("id", id);
-
-		return "redirect:/labplan/private/echantillons/voir";
 		
+		if (testUser(utilisateur)) {
+
+			String token = (String) session.getAttribute("TOKEN");
+			token = "Bearer " + token;
+			formEchantillon.setQualification(id);
+			System.out.println(
+					"Numéro de qualification pour enregistrement ech dans form: " + formEchantillon.getQualification());
+			microServiceLab.saveEchantillon(token, formEchantillon);
+
+			redirectAttributes.addAttribute("id", id);
+
+			return "redirect:/labplan/private/echantillons/voir";
+				
+
+			} else {
+
+				return "redirect:/labplan/connexion";
+			}
+		
+
 	}
 
 	@GetMapping("/voir/{id}")
 	public String listeEchantillons(@PathVariable(name = "id") Integer id, Model model, HttpSession session) {
 
 		Utilisateur utilisateur = userConnexion.obtenirUtilisateur(session, model);
-		String token = (String) session.getAttribute("TOKEN");
-		token = "Bearer " + token;
-		List<EchantillonAux> echantillons = microServiceLab.obtenirEchantillonsParQualification(token, id);
-		model.addAttribute("echantillons", echantillons);
-		QualificationAux qualif = microServiceLab.obtenirQualification(token, id);
-		model.addAttribute("qualification", qualif);
+		
 
-		return Constants.ECHANTILLONS;
+		if (testUser(utilisateur)) {
 
+			String token = (String) session.getAttribute("TOKEN");
+			token = "Bearer " + token;
+			List<EchantillonAux> echantillons = microServiceLab.obtenirEchantillonsParQualification(token, id);
+			model.addAttribute("echantillons", echantillons);
+			QualificationAux qualif = microServiceLab.obtenirQualification(token, id);
+			model.addAttribute("qualification", qualif);
+			return Constants.ECHANTILLONS;
+
+				
+
+			} else {
+
+				return "redirect:/labplan/connexion";
+			}
+	
 	}
 
 	@GetMapping("/desactiver/{id}/{qualification}")
-	public String desactiverEchatillon(@PathVariable(name = "id") Integer id, @PathVariable(name = "qualification") Integer qualification
-			, Model model
-			, HttpSession session
-			, RedirectAttributes redirectAttributes) {
+	public String desactiverEchatillon(@PathVariable(name = "id") Integer id,
+			@PathVariable(name = "qualification") Integer qualification, Model model, HttpSession session,
+			RedirectAttributes redirectAttributes) {
 
 		Utilisateur utilisateur = userConnexion.obtenirUtilisateur(session, model);
-		String token = (String) session.getAttribute("TOKEN");
-		token = "Bearer " + token;
-		microServiceLab.desactiverEchantillon(token, id, qualification);
-		redirectAttributes.addAttribute("id", qualification);
-		List<EchantillonAux> echantillons = microServiceLab.obtenirEchantillonsParQualification(token, qualification);
-		model.addAttribute("echantillons", echantillons);
-		QualificationAux qualif = microServiceLab.obtenirQualification(token, qualification);
-		model.addAttribute("qualification", qualif);
+	
+
+		if (testUser(utilisateur)) {
+
+			String token = (String) session.getAttribute("TOKEN");
+			token = "Bearer " + token;
+			microServiceLab.desactiverEchantillon(token, id, qualification);
+			redirectAttributes.addAttribute("id", qualification);
+			List<EchantillonAux> echantillons = microServiceLab.obtenirEchantillonsParQualification(token, qualification);
+			model.addAttribute("echantillons", echantillons);
+			QualificationAux qualif = microServiceLab.obtenirQualification(token, qualification);
+			model.addAttribute("qualification", qualif);
+			return "redirect:/labplan/private/echantillons/voir";
+				
+
+			} else {
+
+				return "redirect:/labplan/connexion";
+			}
 		
-		return "redirect:/labplan/private/echantillons/voir";
 
 	}
-	
+
 	@GetMapping("/voir")
 	public String listeEchantillons2(@RequestParam(name = "id") Integer id, Model model, HttpSession session) {
 
 		Utilisateur utilisateur = userConnexion.obtenirUtilisateur(session, model);
-		String token = (String) session.getAttribute("TOKEN");
-		token = "Bearer " + token;
-		List<EchantillonAux> echantillons = microServiceLab.obtenirEchantillonsParQualification(token, id);
-		model.addAttribute("echantillons", echantillons);
-		QualificationAux qualif = microServiceLab.obtenirQualification(token, id);
-		model.addAttribute("qualification", qualif);
+		
 
-		return Constants.ECHANTILLONS;
+		if (testUser(utilisateur)) {
 
+			String token = (String) session.getAttribute("TOKEN");
+			token = "Bearer " + token;
+			List<EchantillonAux> echantillons = microServiceLab.obtenirEchantillonsParQualification(token, id);
+			model.addAttribute("echantillons", echantillons);
+			QualificationAux qualif = microServiceLab.obtenirQualification(token, id);
+			model.addAttribute("qualification", qualif);
+			return Constants.ECHANTILLONS;
+
+
+			} else {
+
+				return "redirect:/labplan/connexion";
+			}
+		
 	}
-	
-	
+
 	@GetMapping("/activer/{id}/{qualification}")
-	public String activerEchatillon(
-			@PathVariable(name = "id") Integer id
-			, @PathVariable(name = "qualification") Integer qualification
-			, Model model
-			, HttpSession session
-			, RedirectAttributes redirectAttributes) {
-
-		Utilisateur utilisateur = userConnexion.obtenirUtilisateur(session, model);
-		String token = (String) session.getAttribute("TOKEN");
-		token = "Bearer " + token;
-		microServiceLab.activerEchantillon(token, id, qualification);
-		redirectAttributes.addAttribute("id", qualification);
-		List<EchantillonAux> echantillons = microServiceLab.obtenirEchantillonsParQualification(token, qualification);
-		model.addAttribute("echantillons", echantillons);
-		QualificationAux qualif = microServiceLab.obtenirQualification(token, qualification);
-		model.addAttribute("qualification", qualif);
-		
-		return "redirect:/labplan/private/echantillons/voir";
-
-	}
-	
-	@GetMapping("/modifier/{id}/{qualification}")
-	public String modifierEchantillon(
-			@PathVariable(name = "id") Integer id,
-			@PathVariable(name = "qualification") Integer qualification,
-			Model model,
-			HttpSession session) {
-		
-		
-		Utilisateur utilisateur = userConnexion.obtenirUtilisateur(session, model);
-		String token = (String) session.getAttribute("TOKEN");
-		token = "Bearer " + token;
-		EchantillonAux echantillon = microServiceLab.obtenirEchantillon(token, id);
-		
-		FormEchantillon formEchantillon = new FormEchantillon();
-		formEchantillon.setCaracteristique(echantillon.getCaracteristique());
-		formEchantillon.setNumero(echantillon.getNumero());
-		formEchantillon.setVersion(echantillon.getVersion());
-		formEchantillon.setId(id);
-		
-		String date = echantillon.getDate();
-		
-		System.out.println("Date échantillon récupérée:" + date);
-		
-		model.addAttribute("formEchantillon", formEchantillon);
-		model.addAttribute("echantillon", id);
-		model.addAttribute("qualification", qualification);
-		
-		return Constants.MODIFIER_ECHANTILLON;
-		
-	}
-	
-	@PostMapping("/modifier/{id}/{qualification}")
-	public String enregistrerEchantillon(
-			@PathVariable(name = "id") Integer id,
-			@PathVariable(name = "qualification") Integer qualification,
-			Model model,
-			HttpSession session,
-			FormEchantillon formEchantillon,
+	public String activerEchatillon(@PathVariable(name = "id") Integer id,
+			@PathVariable(name = "qualification") Integer qualification, Model model, HttpSession session,
 			RedirectAttributes redirectAttributes) {
-		
+
 		Utilisateur utilisateur = userConnexion.obtenirUtilisateur(session, model);
-		String token = (String) session.getAttribute("TOKEN");
-		token = "Bearer " + token;
-		formEchantillon.setId(id);
-		microServiceLab.modifierEchantillon(token, formEchantillon);
-		System.out.println("id récupéré: " + id);
-		System.out.println("qualif récupéré: " + qualification);
 		
-		redirectAttributes.addAttribute("id", qualification);
-		List<EchantillonAux> echantillons = microServiceLab.obtenirEchantillonsParQualification(token, qualification);
-		model.addAttribute("echantillons", echantillons);
-		QualificationAux qualif = microServiceLab.obtenirQualification(token, qualification);
-		model.addAttribute("qualification", qualif);
+
+		if (testUser(utilisateur)) {
+
+			String token = (String) session.getAttribute("TOKEN");
+			token = "Bearer " + token;
+			microServiceLab.activerEchantillon(token, id, qualification);
+			redirectAttributes.addAttribute("id", qualification);
+			List<EchantillonAux> echantillons = microServiceLab.obtenirEchantillonsParQualification(token, qualification);
+			model.addAttribute("echantillons", echantillons);
+			QualificationAux qualif = microServiceLab.obtenirQualification(token, qualification);
+			model.addAttribute("qualification", qualif);
+			return "redirect:/labplan/private/echantillons/voir";
+				
+
+			} else {
+
+				return "redirect:/labplan/connexion";
+			}
 		
-		return "redirect:/labplan/private/echantillons/voir";
+
+	}
+
+	@GetMapping("/modifier/{id}/{qualification}")
+	public String modifierEchantillon(@PathVariable(name = "id") Integer id,
+			@PathVariable(name = "qualification") Integer qualification, Model model, HttpSession session) {
+
+		Utilisateur utilisateur = userConnexion.obtenirUtilisateur(session, model);
+		
+		if (testUser(utilisateur)) {
+			
+			String token = (String) session.getAttribute("TOKEN");
+			token = "Bearer " + token;
+			EchantillonAux echantillon = microServiceLab.obtenirEchantillon(token, id);
+
+			FormEchantillon formEchantillon = new FormEchantillon();
+			formEchantillon.setCaracteristique(echantillon.getCaracteristique());
+			formEchantillon.setNumero(echantillon.getNumero());
+			formEchantillon.setVersion(echantillon.getVersion());
+			formEchantillon.setId(id);
+
+			String date = echantillon.getDate();
+
+			System.out.println("Date échantillon récupérée:" + date);
+
+			model.addAttribute("formEchantillon", formEchantillon);
+			model.addAttribute("echantillon", id);
+			model.addAttribute("qualification", qualification);
+
+			return Constants.MODIFIER_ECHANTILLON;
+
+				
+
+			} else {
+
+				return "redirect:/labplan/connexion";
+			}
+		
+	}
+
+	@PostMapping("/modifier/{id}/{qualification}")
+	public String enregistrerEchantillon(@PathVariable(name = "id") Integer id,
+			@PathVariable(name = "qualification") Integer qualification, Model model, HttpSession session,
+			FormEchantillon formEchantillon, RedirectAttributes redirectAttributes) {
+
+		Utilisateur utilisateur = userConnexion.obtenirUtilisateur(session, model);
+		
+
+		if (testUser(utilisateur)) {
+
+			String token = (String) session.getAttribute("TOKEN");
+			token = "Bearer " + token;
+			formEchantillon.setId(id);
+			microServiceLab.modifierEchantillon(token, formEchantillon);
+			System.out.println("id récupéré: " + id);
+			System.out.println("qualif récupéré: " + qualification);
+
+			redirectAttributes.addAttribute("id", qualification);
+			List<EchantillonAux> echantillons = microServiceLab.obtenirEchantillonsParQualification(token, qualification);
+			model.addAttribute("echantillons", echantillons);
+			QualificationAux qualif = microServiceLab.obtenirQualification(token, qualification);
+			model.addAttribute("qualification", qualif);
+			return "redirect:/labplan/private/echantillons/voir";
+				
+
+			} else {
+
+				return "redirect:/labplan/connexion";
+			}
+		
+		
+	}
+
+	public boolean testUser(Utilisateur utilisateur) {
+
+		if (utilisateur == null) {
+
+			return false;
+
+		} else
+
+			return true;
 	}
 }
--- src/main/java/com/michel/lab/controller/EssaiController.java
+++ src/main/java/com/michel/lab/controller/EssaiController.java
@@ -38,26 +38,46 @@ public class EssaiController {
 	@GetMapping("/creation")
 	public String creationEssai(Model model, HttpSession session) {
 		Utilisateur utilisateur = userConnexion.obtenirUtilisateur(session, model);
-		model.addAttribute("formEssai", new FormEssai());
-		return Constants.CREATION_ESSAI;
+		
+		
+		if (testUser(utilisateur)) {
+			
+			model.addAttribute("formEssai", new FormEssai());
+			return Constants.CREATION_ESSAI;
+				
+
+			} else {
+
+				return "redirect:/labplan/connexion";
+			}
+		
 	}
 
 	@GetMapping("/choix/{id}")
 	public String choisir(@PathVariable(name = "id") Integer id, Model model, HttpSession session) {
 
 		Utilisateur utilisateur = userConnexion.obtenirUtilisateur(session, model);
-		String token = (String) session.getAttribute("TOKEN");
-		token = "Bearer " + token;
-		QualificationAux qualification = microServiceLab.obtenirQualification(token, id);
-		List<ProcedureAux> procedures = microServiceLab.obtenirProcedures(token);
-		List<DomaineAux> domaines = microServiceLab.obtenirDomaines(token);
-		System.out.println("Taille liste procedures: " + procedures.size());
-		System.out.println("Taille liste domaines: " + domaines.size());
+		
 
-		model.addAttribute("domaines", domaines);
-		model.addAttribute("qualification", id);
+		if (testUser(utilisateur)) {
 
-		return Constants.DOMAINES;
+			String token = (String) session.getAttribute("TOKEN");
+			token = "Bearer " + token;
+			QualificationAux qualification = microServiceLab.obtenirQualification(token, id);
+			List<ProcedureAux> procedures = microServiceLab.obtenirProcedures(token);
+			List<DomaineAux> domaines = microServiceLab.obtenirDomaines(token);
+			System.out.println("Taille liste procedures: " + procedures.size());
+			System.out.println("Taille liste domaines: " + domaines.size());
+
+			model.addAttribute("domaines", domaines);
+			model.addAttribute("qualification", id);
+			return Constants.DOMAINES;
+
+			} else {
+
+				return "redirect:/labplan/connexion";
+			}
+		
 
 	}
 
@@ -67,6 +87,7 @@ public class EssaiController {
 		
 		System.out.println("*** entrée méthode choisirEssais ***");
 		Utilisateur utilisateur = userConnexion.obtenirUtilisateur(session, model);
+		/*
 		String token = (String) session.getAttribute("TOKEN");
 		token = "Bearer " + token;
 
@@ -132,8 +153,83 @@ public class EssaiController {
 		model.addAttribute("qualification", qualification);
 		model.addAttribute("utilisateur", utilisateur);
 		model.addAttribute("domaine", id);
-		return Constants.ESSAIS;
+		*/
+		if (testUser(utilisateur)) {
 
+			String token = (String) session.getAttribute("TOKEN");
+			token = "Bearer " + token;
+
+			System.out.println("id domaine: " + id);
+			System.out.println("id qualification: " + qualification);
+
+			List<ProcedureAux> procedures0 = microServiceLab.obtenirProceduresParDomaine(token, id);  // liste de toutes les procédures du domaine
+			List<Integer> listeIdProcedures = new ArrayList<Integer>();
+
+			for (ProcedureAux pro : procedures0) {  // Récupération de tous les id de toutes les procédures existantes dans le domaine
+
+				Integer idPro = pro.getId();
+				listeIdProcedures.add(idPro);
+			}
+			
+			System.out.println("Taille liste listeIdProcedure: " + listeIdProcedures.size());
+			
+			Groupe groupe = new Groupe(id, qualification);
+			List<Integer> idProcedures = microServiceLab.obtenirSelectionProcedure(token, groupe); // procédures sélectionnées pour
+																							// la qualification
+			System.out.println("Taille liste des procédures sélectionnées pour la qualification choisie: " + idProcedures.size());
+			
+			for(Integer iden: idProcedures) {
+				
+				System.out.println("identifiant de procédure sélectionné: " + iden);
+			}
+			List<ProcedureAux> procedures = new ArrayList<ProcedureAux>();
+			//boolean isPresent ;
+			
+			for (Integer proId : listeIdProcedures) {
+
+				//Integer identifiantProcedure = proId;
+				System.out.println("identifiantProcedure boucle for: " + proId);
+			//	isPresent = idProcedures.contains(identifiantProcedure);
+				
+				if (idProcedures.contains(proId)) {
+					
+					ProcedureAux procedure = procedures0.get(listeIdProcedures.indexOf(proId));
+					procedure.setActif(false);
+					procedures.add(procedure);
+					//pro.setActif(true);
+					
+				} else {
+					
+					ProcedureAux procedure = procedures0.get(listeIdProcedures.indexOf(proId));
+					procedure.setActif(true);
+					procedures.add(procedure);
+					//pro.setActif(false);
+				}
+				
+				//procedures.add(pro);
+				
+
+			}
+			
+			for (ProcedureAux pro: procedures) {
+				
+				System.out.println(pro.toString());
+			}
+			System.out.println("taille liste procedures : " + procedures0.size());
+			System.out.println("taille liste idProcedure des id de procédures sélectionnées : " + idProcedures.size());
+			model.addAttribute("procedures", procedures);
+			model.addAttribute("qualification", qualification);
+			model.addAttribute("utilisateur", utilisateur);
+			model.addAttribute("domaine", id);
+			return Constants.ESSAIS;
+
+				
+
+			} else {
+
+				return "redirect:/labplan/connexion";
+			}
+		
 	}
 
 	@GetMapping("/procedures/ajouter/{id}/{domaine}/{qualification}")
@@ -143,16 +239,26 @@ public class EssaiController {
 			Model model, HttpSession session) {
 
 		Utilisateur utilisateur = userConnexion.obtenirUtilisateur(session, model);
-		String token = (String) session.getAttribute("TOKEN");
-		token = "Bearer " + token;
-		Integer idUser = utilisateur.getId();
-		microServiceLab.ajouterProcedure(token, id, qualification, idUser);
-
-		redirectAttributes.addAttribute("id", id);
-		redirectAttributes.addAttribute("domaine", domaineId);
-		redirectAttributes.addAttribute("qualification", qualification);
+	
 		
-		return "redirect:/labplan/private/essai/procedures";
+		if (testUser(utilisateur)) {
+
+			String token = (String) session.getAttribute("TOKEN");
+			token = "Bearer " + token;
+			Integer idUser = utilisateur.getId();
+			microServiceLab.ajouterProcedure(token, id, qualification, idUser);
+
+			redirectAttributes.addAttribute("id", id);
+			redirectAttributes.addAttribute("domaine", domaineId);
+			redirectAttributes.addAttribute("qualification", qualification);
+			return "redirect:/labplan/private/essai/procedures";
+				
+
+			} else {
+
+				return "redirect:/labplan/connexion";
+			}
+		
 	}
 
 	@GetMapping("/procedures") // id = domaine, qualification = numéro de qualification
@@ -164,75 +270,97 @@ public class EssaiController {
 
 		Utilisateur utilisateur = userConnexion.obtenirUtilisateur(session, model);
 		String token = (String) session.getAttribute("TOKEN");
-		token = "Bearer " + token;
-		System.out.println("*** méthode choisoirEssai2 *** ");
-		System.out.println("Avant");
-		System.out.println("id domaine: " + domaineId);
-		System.out.println("id qualification: " + qualification);
-
-		List<ProcedureAux> procedures0 = microServiceLab.obtenirProceduresParDomaine(token, domaineId);  // liste de toutes les procédures du domaine
-		List<Integer> listeIdProcedures = new ArrayList<Integer>();
-
-		for (ProcedureAux pro : procedures0) {  // Récupération de tous les id de toutes les procédures existantes dans le domaine
-
-			Integer idPro = pro.getId();
-			listeIdProcedures.add(idPro);
-		}
 		
-		System.out.println("Taille liste listeIdProcedure: " + listeIdProcedures.size());
 		
-		Groupe groupe = new Groupe(domaineId, qualification);
-		List<Integer> idProcedures = microServiceLab.obtenirSelectionProcedure(token, groupe); // procédures sélectionnées pour
-																						// la qualification
-		System.out.println("Taille liste des procédures sélectionnées pour la qualification choisie: " + idProcedures.size());
-		
-		for(Integer iden: idProcedures) {
-			
-			System.out.println("identifiant de procédure sélectionné: " + iden);
-		}
-		List<ProcedureAux> procedures = new ArrayList<ProcedureAux>();
-		//boolean isPresent ;
-		
-		for (Integer proId : listeIdProcedures) {
+		if (testUser(utilisateur)) {
 
-			//Integer identifiantProcedure = proId;
-			System.out.println("identifiantProcedure boucle for: " + proId);
-		//	isPresent = idProcedures.contains(identifiantProcedure);
-			
-			if (idProcedures.contains(proId)) {
-				
-				ProcedureAux procedure = procedures0.get(listeIdProcedures.indexOf(proId));
-				procedure.setActif(false);
-				procedures.add(procedure);
-				//pro.setActif(true);
-				
-			} else {
-				
-				ProcedureAux procedure = procedures0.get(listeIdProcedures.indexOf(proId));
-				procedure.setActif(true);
-				procedures.add(procedure);
-				//pro.setActif(false);
+			token = "Bearer " + token;
+			System.out.println("*** méthode choisoirEssai2 *** ");
+			System.out.println("Avant");
+			System.out.println("id domaine: " + domaineId);
+			System.out.println("id qualification: " + qualification);
+
+			List<ProcedureAux> procedures0 = microServiceLab.obtenirProceduresParDomaine(token, domaineId);  // liste de toutes les procédures du domaine
+			List<Integer> listeIdProcedures = new ArrayList<Integer>();
+
+			for (ProcedureAux pro : procedures0) {  // Récupération de tous les id de toutes les procédures existantes dans le domaine
+
+				Integer idPro = pro.getId();
+				listeIdProcedures.add(idPro);
 			}
 			
-			//procedures.add(pro);
+			System.out.println("Taille liste listeIdProcedure: " + listeIdProcedures.size());
 			
-
-		}
-		
-		for (ProcedureAux pro: procedures) {
+			Groupe groupe = new Groupe(domaineId, qualification);
+			List<Integer> idProcedures = microServiceLab.obtenirSelectionProcedure(token, groupe); // procédures sélectionnées pour
+																							// la qualification
+			System.out.println("Taille liste des procédures sélectionnées pour la qualification choisie: " + idProcedures.size());
 			
-			System.out.println(pro.toString());
-		}
-		
-		System.out.println("taille liste procedures : " + procedures0.size());
-		System.out.println("taille liste idProcedure des id de procédures sélectionnées : " + idProcedures.size());
-		
-		model.addAttribute("procedures", procedures);
-		model.addAttribute("qualification", qualification);
-		model.addAttribute("utilisateur", utilisateur);
-		model.addAttribute("domaine", domaineId);
-		return Constants.ESSAIS;
+			for(Integer iden: idProcedures) {
+				
+				System.out.println("identifiant de procédure sélectionné: " + iden);
+			}
+			List<ProcedureAux> procedures = new ArrayList<ProcedureAux>();
+			//boolean isPresent ;
+			
+			for (Integer proId : listeIdProcedures) {
 
+				//Integer identifiantProcedure = proId;
+				System.out.println("identifiantProcedure boucle for: " + proId);
+			//	isPresent = idProcedures.contains(identifiantProcedure);
+				
+				if (idProcedures.contains(proId)) {
+					
+					ProcedureAux procedure = procedures0.get(listeIdProcedures.indexOf(proId));
+					procedure.setActif(false);
+					procedures.add(procedure);
+					//pro.setActif(true);
+					
+				} else {
+					
+					ProcedureAux procedure = procedures0.get(listeIdProcedures.indexOf(proId));
+					procedure.setActif(true);
+					procedures.add(procedure);
+					//pro.setActif(false);
+				}
+				
+				//procedures.add(pro);
+				
+
+			}
+			
+			for (ProcedureAux pro: procedures) {
+				
+				System.out.println(pro.toString());
+			}
+			
+			System.out.println("taille liste procedures : " + procedures0.size());
+			System.out.println("taille liste idProcedure des id de procédures sélectionnées : " + idProcedures.size());
+			
+			model.addAttribute("procedures", procedures);
+			model.addAttribute("qualification", qualification);
+			model.addAttribute("utilisateur", utilisateur);
+			model.addAttribute("domaine", domaineId);
+			return Constants.ESSAIS;
+				
+
+			} else {
+
+				return "redirect:/labplan/connexion";
+			}
+		
+
+	}
+	
+	public boolean testUser(Utilisateur utilisateur) {
+
+		if (utilisateur == null) {
+
+			return false;
+
+		} else
+
+			return true;
 	}
 
 }
